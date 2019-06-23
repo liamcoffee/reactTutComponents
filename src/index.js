fdf039cf217ddb178723d85bb164bfd66278fde4
Liam Baxter <liam@coffee-design.co.uk>
2019-06-23 17:13:06 +0100

    approval compnent, component children as prop

--- src/index.js
+++ src/index.js
@@ -2,14 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam" timeAgo="Today at 2:15PM" image={faker.image.avatar()} commentTxt={faker.lorem.text()} />
-            <CommentDetail author="Jim" timeAgo="Today at 6:15PM"  image={faker.image.avatar()} commentTxt={faker.lorem.text()}/>
-            <CommentDetail author="Chuck" timeAgo="Today at 5:15PM"  image={faker.image.avatar()} commentTxt={faker.lorem.text()}/>
-            <CommentDetail author="Bill" timeAgo="Today at 9:15AM"  image={faker.image.avatar()} commentTxt={faker.lorem.text()}/>
+               <ApprovalCard>
+                    <CommentDetail 
+                        author="Sam" 
+                        timeAgo="Today at 2:15PM" 
+                        image={faker.image.avatar()} 
+                        commentTxt={faker.lorem.text()} 
+                    />
+               </ApprovalCard>
+           
+            <CommentDetail 
+                author="Jim" 
+                timeAgo="Today at 6:15PM"  
+                image={faker.image.avatar()} 
+                commentTxt={faker.lorem.text()}
+            />
+            <CommentDetail 
+                author="Chuck" 
+                timeAgo="Today at 5:15PM" 
+                image={faker.image.avatar()} 
+                commentTxt={faker.lorem.text()}
+            />
+         
         </div>
     );
 };
